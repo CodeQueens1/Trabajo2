e4dd70f67fff0a1c6cb9148bba89029bbd386708
esttefiCarde <duocuc884@gmail.com>
2024-11-04 01:11:35 -0300

    correcion calendario donde se muestra la fecha en mis datos
    se importan las funciones que permiten que se pueda ver el calendario en la parte de fecha de nacimiento de mis datos, se le cambia el nombre al programa por  sistema de asistencia DUOC

--- src/app/components/misdatos/misdatos.component.ts
+++ src/app/components/misdatos/misdatos.component.ts
@@ -5,6 +5,10 @@ import { AuthService } from 'src/app/services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { NivelEducacional } from 'src/app/model/nivel-educacional';
 import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-misdatos',
@@ -12,7 +16,10 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./misdatos.component.scss'],
   standalone:true,
   imports: [
-    IonicModule,FormsModule,CommonModule]
+    IonicModule,FormsModule,CommonModule, MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,]
 })
 export class MisdatosComponent  implements OnInit {
   public usuario: Usuario = new Usuario();
