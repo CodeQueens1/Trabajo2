8cb3d9a7605f178a4a01102db01b612106e0f7eb
esttefiCarde <duocuc884@gmail.com>
2024-11-13 00:34:58 -0300

    que aparezca la contraseña
    que aparezca la contraseña del usuario en la pagina de correcto

--- src/app/pages/correcto/correcto.page.ts
+++ src/app/pages/correcto/correcto.page.ts
@@ -6,6 +6,7 @@ import { Usuario } from 'src/app/model/usuario';
 import { Router } from '@angular/router';
 import { HeaderComponent } from 'src/app/components/header/header.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-correcto',
@@ -19,18 +20,17 @@ export class CorrectoPage implements OnInit {
 usuario: Usuario = new Usuario();
 password: string | undefined
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
 
-    const navigation = this.router.getCurrentNavigation();
-    this.usuario = navigation?.extras.state?.['usuario']; // Obtener el usuario pasado
-
-    if (this.usuario) {
-      this.password = this.usuario.password; // Captura la contraseña del usuario
-    } else {
-      console.log('No se encontró información del usuario.');
-    }
+    this.authService.usuarioAutenticado.subscribe( (usuarioAutenticado)=>{
+      
+      if(usuarioAutenticado){
+         this.usuario = usuarioAutenticado
+        console.log('Usuario cargado en PreguntaPage:', this.usuario);
+      }
+    });
   }
 
   
