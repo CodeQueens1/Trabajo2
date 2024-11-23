d5c6fb97f5a127ec2ea0f2b0336bfed97e6910a5
CodeQueens1 <duocuc884@gmail.com>
2024-11-23 14:09:50 -0300

    Delete Downloads/Trabajo2-main (5)/Trabajo2-main directory

--- Downloads/Trabajo2-main (5)/Trabajo2-main/run.bat
+++
@@ -1,1 +0,0 @@
-ionic cap run android -l --external --host=10.31.225.223 --port=8100 --target ZY22HXNBHZ
--- Downloads/Trabajo2-main (5)/Trabajo2-main/start.sh
+++
@@ -1,1 +0,0 @@
-ionic cap run android -l --external --host=10.20.5.85 --port=8100 --target GAPF8TZLKZU8GMZ5
